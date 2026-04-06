078e82fc449cdcd743f1d7266d026dbf5ca64e6a
RemiFourmentin <remi.fourmentin@edu.devinci.fr>
2026-04-07 00:04:59 +0200

    Ajout systeme de combat

--- Alterdune_Code/Alterdune_Code/Alterdune_Code.cpp
+++ Alterdune_Code/Alterdune_Code/Alterdune_Code.cpp
@@ -5,11 +5,66 @@
 #include "item.h"
 #include "monster.h"
 #include "Joueur.h"
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 void MenuPrincipal(Joueur j, Item* tabitems, Monster* tabmonsters);
 
-void Bestiaire(Joueur j, Item* tabitems, Monster* tabmonsters){
+void Fight(Joueur j, Item* tabitems, Monster* tabmonsters, Monster monstre) {
+	int degats = rand() % monstre.getStatistiques()[0];
+	if (degats == 0) {
+		cout << "Raté... " << monstre.getNom() << " ne prend aucun degats";
+	}
+	if (degats > monstre.getStatistiques()[0] * 0.8 && monstre.getHp() > monstre.getStatistiques()[0] * 0.6) {
+		cout << "Arghh... " << monstre.getNom() << " a ete tranche dans le vif";
+	}
+	monstre.Degats(degats);
+	if (monstre.getHp() == 0) {
+		j.Tue(monstre);
+	}
+}
+
+void Act(Joueur j, Item* tabitems, Monster* tabmonsters, Monster monstre){
+
+}
+
+void UseItem(Joueur j, Item* tabitems, Monster* tabmonsters, Monster monstre){}
+
+void Mercy(Joueur j, Item* tabitems, Monster* tabmonsters, Monster monstre) {}
+
+void Combat(Joueur j, Item* tabitems, Monster* tabmonsters){
+	int random = rand() % tabmonsters->getCompteur();
+	Monster monstre = tabmonsters[random];
+	char c = ' ';
+	while (j.getHp() > 0 || monstre.getHp() > 0 || j.getStatistiques()[3] < monstre.getMercyGoal()) {
+		cout << "FIGHT [F]" << endl << "ACT [A]" << endl << "ITEM [I]" << endl << "Mercy [M]" << endl;
+		cin >> c;
+		system("cls");
+		switch(c){
+			case 'F':
+				Fight(j, tabitems, tabmonsters, monstre);
+				break;
+
+			case 'A':
+				Act(j, tabitems, tabmonsters, monstre);
+				break;
+
+			case 'I':
+				UseItem(j, tabitems, tabmonsters, monstre);
+				break;
+
+			case 'M':
+				Mercy(j, tabitems, tabmonsters, monstre);
+				break;
+
+			default:
+				break;
+		}
+	}
+}
+
+void Bestiaire(Joueur j, Item* tabitems, Monster* tabmonsters) {
 
 	std::cout << "BESTIAIRE" << endl << endl;
 	for (int i = 0; i < j.getBestiaire()->getCompteur(); i++) {
@@ -23,8 +78,6 @@ void Bestiaire(Joueur j, Item* tabitems, Monster* tabmonsters){
 	system("cls");
 	MenuPrincipal(j, tabitems, tabmonsters);
 }
-
-void Combat(Joueur j, Item* tabitems, Monster* tabmonsters){}
 
 void Statistiques(Joueur j, Item* tabitems, Monster* tabmonsters) {
 
@@ -136,6 +189,7 @@ void LancementJeu() {
 	Joueur j(nom);
 	Item* tabitems = RecupItems();
 	Monster* tabmonsters = RecupMonsters();
+	srand(time(0));
 	system("cls");
 	MenuPrincipal(j, tabitems, tabmonsters);
 }
